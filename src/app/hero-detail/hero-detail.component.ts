f94a3f59787c6dc91831eef7fd50e64c77a0d475
dchep <dchep@localhost>
2020-01-28 17:14:56 +0200

    http

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -35,4 +35,9 @@ export class HeroDetailComponent implements OnInit {
   goBack(): void {
     this.location.back();
   }
+
+  save(): void {
+     this.heroService.updateHero(this.hero)
+       .subscribe(() => this.goBack());
+   }
 }
