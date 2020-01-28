67e8cd9592fc045f3aff6dccdf728f4c49184b67
dchep <dchep@localhost>
2020-01-28 16:15:37 +0200

    routing p1

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
-import { MessageService } from './message.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
