e3944befe4ff2e00b3a2f0089cff4e1a6995ce22
dchep <dchep@localhost>
2020-01-28 16:33:45 +0200

    routing p2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,7 +12,6 @@ import { MessageService } from '../message.service';
 
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) { }
 
@@ -20,13 +19,8 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
   getHeroes(): void {
     this.heroService.getHeroes()
-          .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
