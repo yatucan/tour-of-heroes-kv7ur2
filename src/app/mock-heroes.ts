b82545a051704d47df19bae1ca1e96a63c9efda5
dchep <dchep@localhost>
2020-02-07 15:20:38 +0200

    updated mock hero

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Dr Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 11, name: 'Dr Nice' ,   tid: '099-142-4321'},
+  { id: 12, name: 'Narco'   ,   tid: '099-242-4321'},
+  { id: 13, name: 'Bombasto',   tid: '099-342-4321'},
+  { id: 14, name: 'Celeritas',  tid: '099-442-4321'},
+  { id: 15, name: 'Magneta',    tid: '099-542-4321'},
+  { id: 16, name: 'RubberMan',  tid: '099-642-4321'},
+  { id: 17, name: 'Dynama'  ,   tid: '099-742-4321'},
+  { id: 18, name: 'Dr IQ'   ,   tid: '099-842-4321'},
+  { id: 19, name: 'Magma'   ,   tid: '099-942-4321'},
+  { id: 20, name: 'Tornado' ,   tid: '099-042-4321'}
 ];
